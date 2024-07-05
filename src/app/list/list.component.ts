69a02b78ca0fdc3c41532662b51998a1b188c0a1
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2024-07-05 01:02:30 -1200

    last

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,17 +1,17 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
-
+import {MatTableModule} from '@angular/material/table';
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule,MatTableModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
 http =inject(HttpClient);
 data: any;
-
+displayedColumns: string[] = ['id', 'name', 'email'];
 ngOnInit(){
 this.http.get("https://jsonplaceholder.typicode.com/comments").subscribe( x => {
   this.data =x;
